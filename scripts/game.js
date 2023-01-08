79cb037d725a3c4953984890f28d7c2b1acfcfa6
Paul N Barnes <paulieb99@gmail.com>
2023-01-07 22:16:46 -0800

    fixed bug within for loop that did not increment the loop variable and had the wrong variable in the nested for loop. once bug found, the border walls rendered on index.html page

--- scripts/game.js
+++ scripts/game.js
@@ -1,11 +1,13 @@
 const canvas = document.getElementById('canvas');
 // ? getContext Method, setting the drawing context for the canvas element in index.html, setting canvas as 2 dimensional object https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/getContext
 const canvasContext = canvas.getContext('2d');
-
+let fps = 30;
+let oneBlockSize = 20;
+let borderColor = '#0900FF';
 const pacmanFrames = document.getElementById('pacmanAnimation');
 const ghostFrames = document.getElementById('ghosts');
 
-let createRect = (x, y, width, height) => {
+let createRect = (x, y, width, height, color) => {
   canvasContext.fillStyle = color;
   //   ? fillRect will be the method to draw the rectangle with the below dimensions
   canvasContext.fillRect(x, y, width, height);
@@ -43,3 +45,26 @@ let gameLoop = () => {
 };
 
 let update = () => {};
+
+let draw = () => {
+  drawBorders();
+};
+
+let gameInterval = setInterval(gameLoop, 1000 / fps);
+
+let drawBorders = () => {
+  for (i = 0; i < map.length; i++) {
+    for (let j = 0; j < map[0].length; j++) {
+      // if the current index is a 1, then it is a wall, 1 = border
+      if (map[i][j] == 1) {
+        createRect(
+          j * oneBlockSize,
+          i * oneBlockSize,
+          oneBlockSize,
+          oneBlockSize,
+          borderColor
+        );
+      }
+    }
+  }
+};
